ab96b37771a9d0f22b631597a0eda40d1266bf98
Jonathan Dubin <mail@jondubin.com>
2015-12-02 19:58:15 -0500

    app

--- populate_db.py
+++ populate_db.py
@@ -1,5 +1,30 @@
 import json
-# from pprint import pprint
+from sqlalchemy import (Table, Column, Integer,
+                        String, Unicode, MetaData,
+                        ForeignKey, create_engine)
+from config import engine_url
+
+metadata = MetaData()
+
+actors = Table('actors', metadata,
+               Column('actor_id', Integer, primary_key=True),
+               Column('name', Unicode(100))
+)
+
+movies = Table('movies', metadata,
+               Column('movie_id', Integer, primary_key=True),
+               Column('name', Unicode(100))
+)
+
+acts = Table('acts', metadata,
+             Column('actor_id', Integer, ForeignKey('actors.actor_id')),
+             Column('movie_id', Integer, ForeignKey('movies.movie_id'))
+)
+
+engine = create_engine(engine_url, echo=True)
+
+def create_all():
+    metadata.create_all(engine)
 
 
 def get_json(filename):
@@ -11,36 +36,54 @@ def get_json(filename):
             char = data_file.read(1)
             if not char:
                 break
-            if char.isspace():
-                continue
             if char == "{":
                 depth += 1
             elif char == "}":
                 depth -= 1
             record_str += char
             if depth == 0:
+                if record_str.isspace():
+                    continue
+                print record_str
                 record = json.loads(record_str)
                 records.append(record)
                 record_str = ""
     return records
 
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in xrange(0, len(l), n):
+        yield l[i:i+n]
+
+def insert_in_chunks(table, items_to_insert):
+    conn = engine.connect()
+    for chunk in chunks(items_to_insert, 5000):
+        conn.execute(table.insert(), chunk)
 
 def insert_people():
     people = get_json("TMDBPersonInfo")
+    to_insert = []
+
     for person in people:
         name = person["name"]
         person_id = person["personId"]
-        # insert person
+        to_insert.append({"actor_id": person_id, "name": name})
+
+    insert_in_chunks(actors, to_insert)
 
 
 def insert_movies_and_acts():
-    movies = get_json("TMDBMovieInfo")
-    for movie in movies:
-        title = movie["title"]
+    movies_json = get_json("TMDBMovieInfo")
+    to_insert_movies = []
+    to_insert_acts = []
+    for movie in movies_json:
+        name = movie["title"]
         movie_id = movie["id"]
-        # insert movie
-        for actor in movie["cast"]:
-            person_id = actor["personId"]
-            #insert acts
+        to_insert_movies.append({"movie_id": movie_id, "name": name})
 
-# remove non actors
+        for actor in movie["cast"]:
+            actor_id = actor["personId"]
+            to_insert_acts.append({"movie_id": movie_id, "actor_id": actor_id})
+
+    insert_in_chunks(movies, to_insert_movies)
+    insert_in_chunks(acts, to_insert_acts)
