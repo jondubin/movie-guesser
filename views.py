38b8eb33bc9188cd5e237527d5886a3c8f1c52a2
Jonathan Dubin <mail@jondubin.com>
2015-12-08 22:11:47 -0500

    adds column

--- views.py
+++ views.py
@@ -31,7 +31,7 @@ def get_movies_from_id(actor_id):
     results = conn.execute(stmt)
     movie_list = []
     for result in results:
-        print result.decode('cp1252')
+        print result[0].decode('cp1252')
         # movie_list.append(result[0])
     return movie_list
 
